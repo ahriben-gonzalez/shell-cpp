095fc7c275b182eb34b5094b2df403e4df2c61aa
ahriben gonzalez <ahriben.gonzalez@intel.com>
2025-02-07 10:01:43 -0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <string>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::string exit = "exit";
 
-  // Uncomment this block to pass the first stage
   while (true){
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+    if (input.compare(0, exit.size(), exit) == 0) {
+        return 0;
+    }
     std::cout << input << ": command not found" << std::endl;
   }
 }
