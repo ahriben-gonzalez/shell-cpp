ae9ab73ae69e4f255a6c768356e283808dbbcdef
ahriben gonzalez <ahriben.gonzalez@intel.com>
2025-02-07 10:08:25 -0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,22 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   std::string exit = "exit";
+  std::string echo = "echo";
 
   while (true){
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+    
     if (input.compare(0, exit.size(), exit) == 0) {
         return 0;
     }
+    
+    if (input.compare(0, echo.size(), echo) == 0) {
+        std::cout << input.substr(echo.length() + 1) << std::endl;
+        continue;
+    }
+    
     std::cout << input << ": command not found" << std::endl;
   }
 }
