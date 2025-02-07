a6a1ae8f87425150d98c590bcf0f1fa79226cf58
ahriben gonzalez <ahriben.gonzalez@intel.com>
2025-02-07 10:25:23 -0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,49 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <set>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   std::string exit = "exit";
   std::string echo = "echo";
+  std::string type = "type";
+  std::set<std::string> commands = {"exit", "echo", "type"};
 
-  while (true){
+  while (true)
+  {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    
-    if (input.compare(0, exit.size(), exit) == 0) {
-        return 0;
+
+    if (input.compare(0, exit.size(), exit) == 0)
+    {
+      return 0;
     }
-    
-    if (input.compare(0, echo.size(), echo) == 0) {
-        std::cout << input.substr(echo.length() + 1) << std::endl;
-        continue;
+
+    if (input.compare(0, echo.size(), echo) == 0)
+    {
+      std::cout << input.substr(echo.length() + 1) << std::endl;
+      continue;
     }
-    
+
+    if (input.compare(0, type.size(), type) == 0)
+    {
+      std::string cmd = input.substr(echo.length() + 1, input.length() - (type.length() + 1));
+      if (commands.find(cmd) != commands.end())
+      {
+        std::cout <<  cmd << " is a shell builtin" << std::endl;
+      }
+      else
+      {
+        std::cout <<  cmd << ": not found" << std::endl;
+      }
+      continue;
+    }
+
     std::cout << input << ": command not found" << std::endl;
   }
 }
