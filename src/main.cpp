72bd16a7fa771ec67016e1d304a1a7fc7d9064d7
ahriben gonzalez <ahriben.gonzalez@intel.com>
2025-02-10 13:53:42 -0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,131 @@ typedef enum CMDS
   UNKNOWN
 };
 
+class Command
+{
+public:
+  std::vector<std::string> paths;
+
+  Command(std::vector<std::string> _paths)
+  {
+    paths = _paths;
+  }
+
+  virtual int execute(std::string &input)
+  {
+    return 0;
+  }
+
+  virtual bool should_exit()
+  {
+    return false;
+  }
+};
+
+class Exit : public Command
+{
+public:
+  using Command::Command;
+  virtual int execute(std::string &input)
+  {
+    return 0;
+  }
+  virtual bool should_exit()
+  {
+    return true;
+  }
+};
+
+class Echo : public Command
+{
+public:
+  using Command::Command;
+  virtual int execute(std::string &input)
+  {
+    std::cout << input.substr(6) << std::endl;
+    return 0;
+  }
+};
+
+class Type : public Command
+{
+public:
+  using Command::Command;
+  virtual int execute(std::string &input)
+  {
+    std::string cmd = input.substr(6, input.length() - 6);
+    std::string cmdpath = "";
+    bool found = false;
+    /*
+    // check for shell built ins
+    if (commands.find(cmd) != commands.end())
+    {
+      std::cout << cmd << " is a shell builtin" << std::endl;
+      return 0;
+    }
+    */
+
+    // check in PATH dirs
+    for (std::string p : paths)
+    {
+      std::filesystem::path search_path = p;
+      if (exists(search_path, cmd))
+      {
+        cmdpath = p;
+        break;
+      }
+    }
+
+    if (!cmdpath.empty())
+    {
+      std::cout << cmd << " is " << cmdpath << "/" << cmd << std::endl;
+    }
+    else
+    {
+      std::cout << cmd << ": not found" << std::endl;
+    }
+    return 0;
+  }
+};
+
+class Unknown : public Command
+{
+public:
+  using Command::Command;
+  virtual int execute(std::string &input)
+  {
+    std::vector<std::string> splitcmds = split(input, ' ');
+    std::string cmdtype = splitcmds.at(0);
+    std::string cmdpath = "";
+    bool found = false;
+
+    // check in PATH dirs
+    for (std::string p : paths)
+    {
+      std::filesystem::path search_path = p;
+      if (exists(search_path, cmdtype))
+      {
+        cmdpath = p;
+        break;
+      }
+    }
+
+    if (!cmdpath.empty())
+    {
+      // std::string fullcmd = cmdpath + '/' + cmdtype;
+      // splitcmds[0] = fullcmd;
+
+      std::string fullcmd_args = join(splitcmds, " ");
+      system(fullcmd_args.c_str());
+    }
+    else
+    {
+      std::cout << input << ": command not found" << std::endl;
+    }
+    return 0;
+  }
+};
+
 std::vector<std::string> split(const std::string &s, char delim)
 {
   std::vector<std::string> elems;
@@ -25,13 +150,9 @@ std::vector<std::string> split(const std::string &s, char delim)
   return elems;
 }
 
-CMDS getCmd(std::string cmdtype, std::unordered_map<std::string, int> cmds)
+Command getCmd(std::string cmdtype, std::unordered_map<std::string, Command &> cmds)
 {
-  if (cmds.find(cmdtype) == cmds.end())
-  {
-    return UNKNOWN;
-  }
-  return (CMDS)cmds[cmdtype];
+  return (Command)cmds[cmdtype];
 }
 
 bool exists(const std::filesystem::path &path, const std::string &filename)
@@ -52,12 +173,14 @@ bool exists(const std::filesystem::path &path, const std::string &filename)
   return false;
 }
 
-std::string join(const std::vector<std::string>& vec, std::string delimeter) {
-    std::ostringstream stream;
-    for (auto& elem : vec) {
-        stream << elem << delimeter;
-    }
-    return stream.str();
+std::string join(const std::vector<std::string> &vec, std::string delimeter)
+{
+  std::ostringstream stream;
+  for (auto &elem : vec)
+  {
+    stream << elem << delimeter;
+  }
+  return stream.str();
 }
 
 int main()
@@ -66,13 +189,14 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::unordered_map<std::string, int> commands;
-  commands["exit"] = EXIT;
-  commands["echo"] = ECHO;
-  commands["type"] = TYPE;
-
+  std::unordered_map<std::string, Command &> commands;
   const char *path = std::getenv("PATH");
   std::vector<std::string> paths = split(path, ':');
+
+  commands["exit"] = Exit(paths);
+  commands["echo"] = Echo(paths);
+  commands["type"] = Type(paths);
+  commands["unknown"] = Unknown(paths);
 
   while (true)
   {
@@ -83,88 +207,11 @@ int main()
     std::vector<std::string> splitcmds = split(input, ' ');
     std::string cmdtype = splitcmds.at(0);
 
-    CMDS c = getCmd(cmdtype, commands);
-
-    switch (c)
-    {
-    case EXIT:
-    {
+    Command c = getCmd(cmdtype, commands);
+    c.execute(input);
+    if (c.should_exit()){
       return 0;
     }
-    case ECHO:
-    {
-      std::cout << input.substr(cmdtype.length() + 1) << std::endl;
-      break;
-    }
-    case TYPE:
-    {
-      std::string cmd = input.substr(cmdtype.length() + 1, input.length() - (cmdtype.length() + 1));
-      std::string cmdpath = "";
-      bool found = false;
 
-      // check for shell built ins
-      if (commands.find(cmd) != commands.end())
-      {
-        std::cout << cmd << " is a shell builtin" << std::endl;
-        break;
-      }
-
-      // check in PATH dirs
-      for (std::string p : paths)
-      {
-        std::filesystem::path search_path = p;
-        if (exists(search_path, cmd))
-        {
-          cmdpath = p;
-          break;
-        }
-      }
-
-      if (!cmdpath.empty())
-      {
-        std::cout << cmd << " is " << cmdpath << "/" << cmd << std::endl;
-      }
-      else
-      {
-        std::cout << cmd << ": not found" << std::endl;
-      }
-      break;
-    }
-    case UNKNOWN:
-    {
-      std::string cmdpath = "";
-      bool found = false;
-
-      // check in PATH dirs
-      for (std::string p : paths)
-      {
-        std::filesystem::path search_path = p;
-        if (exists(search_path, cmdtype))
-        {
-          cmdpath = p;
-          break;
-        }
-      }
-
-      if (!cmdpath.empty())
-      {
-        //std::string fullcmd = cmdpath + '/' + cmdtype;
-        //splitcmds[0] = fullcmd;
-
-        std::string fullcmd_args = join(splitcmds, " ");
-        system(fullcmd_args.c_str());
-      }
-      else
-      {
-        std::cout << input << ": command not found" << std::endl;
-      }
-
-      break;
-    }
-    default:
-    {
-      break;
-    }
-    }
   }
 }
